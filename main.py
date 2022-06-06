2286ef9415da63d91bf12a603be4ba90e4b2a787
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-06-06 17:47:57 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+
+from time import sleep
+import RPi.GPIO as GPIO
+
+PUL = 17  
+DIR = 27  
+ENA = 22 
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PUL, GPIO.OUT)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(ENA, GPIO.OUT)
+
+def step(step,dir,speed=0.0000001):
+    GPIO.output(ENA, GPIO.HIGH)
+    # sleep(.5) 
+    GPIO.output(DIR, GPIO.LOW)
+    for x in range(step): 
+        GPIO.output(PUL, GPIO.HIGH)
+        sleep(speed)
+        GPIO.output(PUL, GPIO.LOW)
+        sleep(speed)
+    GPIO.output(ENA, GPIO.LOW)
+    sleep(.5)
+    return
+
+def main():
+    istep = int(input('Input step:\n'))
+    istep = bool(input('Input dir (1/0):\n'))
